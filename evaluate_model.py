dc07902df9023fbd2db04f39d322024d2d4ee1f8
baohaoliao <liaobaohao@gmail.com>
2025-04-05 19:30:56 +0200

    evaluate_model

--- evaluate_model.py
+++ evaluate_model.py
@@ -55,6 +55,15 @@ def apply_new_r1_template(question: str):
     )
 
 
+def apply_new_r1_think_template(question: str):
+    return (
+        "A conversation between User and Assistant. The User asks a question, and the Assistant solves it. The Assistant first thinks about the reasoning process in the mind and then provides the User with the answer. "
+        "The reasoning process is enclosed within <think> </think> tags. And the final answer should be placed within \\boxed{}, i.e., <think> reasoning process here </think> \\boxed{answer} here."
+        "\nUser: " + question
+        + "\nAssistant: <think>"
+    )
+
+
 def apply_qwen_r1_template(question: str):
     return (
         "<|im_start|>system\nA conversation between user and assistant. The user asks a question, and the assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the final answer. "
@@ -156,6 +165,9 @@ def main(
     elif template == "new_r1":
         math_reward_fn = boxed_reward_fn
         apply_template = apply_new_r1_template
+    elif template == "new_r1_think":
+        math_reward_fn = boxed_reward_fn
+        apply_template = apply_new_r1_think_template
     elif template == "prime-zero":
         math_reward_fn = boxed_reward_fn
         apply_template = apply_prime_zero_template
